9f998eec226f1cf0b13a60e7093c7a57c369b221
PedroOliveira0609 <90793896+PedroOliveira0609@users.noreply.github.com>
2022-10-01 16:26:00 -0300

    AppenFormat

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using static System.Console;
+using System.Text;
+internal class Program
+{
+     private static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        double moeda = 1234567.9;
+        sb.AppendFormat(moeda.ToString("C", CultureInfo.CurrentCulture));
+        sb.AppendLine();
+        sb.AppendFormat(moeda.ToString("C3", CultureInfo.CreateSpecificCulture("pt-br")));
+        Console.WriteLine(sb.ToString());
+    }
+}
